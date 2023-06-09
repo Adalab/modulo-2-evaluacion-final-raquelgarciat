aa08b25cbc5c039ed8ccda37dfa5e386a7417bb6
raquelgarciat <raquelgarciat@gmail.com>
2023-06-09 22:21:50 +0200

    add JS fetch

---
+++ src/js/01_fetch.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const charListElement = document.querySelector('.character_list');
+let charDataList = [];
+
+function renderCharacter(charData) {
+  const char = `<div class="character_list-one">
+    <img
+      class="char_img"
+      src=${charData.imageUrl}
+      alt="character"
+    />
+    <p class="char_name">${charData.name}</p>
+  </div>`;
+  return char;
+}
+
+function renderCharList(charDataList) {
+  for (const eachCharacter of charDataList) {
+    charListElement.innerHTML += renderCharacter(eachCharacter);
+  }
+}
+
+fetch('https://api.disneyapi.dev/character/')
+  .then((response) => response.json())
+  .then((charData) => {
+    console.log(charData);
+    charDataList = charData.data;
+    renderCharList(charDataList);
+    localStorage.setItem('data', JSON.stringify(charDataList));
+  });
