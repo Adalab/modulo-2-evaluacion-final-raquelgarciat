e6c097ffcaa85013423b487b18cae20cac95dddc
raquelgarciat <raquelgarciat@gmail.com>
2023-06-13 09:35:35 +0200

    add functional search - step1

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -6,7 +6,7 @@ const searchBtn = document.querySelector('.js-search-btn');
 const searchInput = document.querySelector('.js-search-input');
 const searchValue = searchInput.value;
 let url = 'http://api.disneyapi.dev/character?pageSize=20';
-const apiUrl = 'https://api.disneyapi.dev/character';
+const searchUrl = 'https://api.disneyapi.dev/character?pageSize=50&name=${charData.name}';
 const clearBtn = document.querySelector('.clear-btn');
 
 let charDataList = [];
--- src/js/04_search.js
+++ src/js/04_search.js
@@ -1,28 +1,44 @@
 'use strict';
 
-function fetchSearchData() {
-  fetch(apiUrl)
+/*function fetchSearchData() {
+  fetch(searchUrl)
     .then((response) => response.json())
     .then((searchCharData) => {
       apiDataList = searchCharData.data;
     });
-}
-
-function renderSearchList() {
-  charListElement.innerHTML = '';
-  for (const eachSearch of filterList) {
-    charListElement.innerHTML += renderCharacter(eachSearch);
-  }
-}
+}*/
 
 function handleSearch (event) {
   event.preventDefault();
-  filterList = apiDataList.filter((newCharData) =>
+  const searchValue = searchInput.value;
+  const filterList = charDataList.filter((newCharData) =>
     newCharData.name.toLowerCase().includes(searchValue.toLowerCase())
   );
   console.log(searchValue);
-  fetchSearchData();
-  renderSearchList();
+  console.log(filterList);
+  //fetchSearchData();
+  renderSearchList(filterList);
 };
+
+function renderSearchCharacter(newCharData) {
+  let html = `<li id="${newCharData._id}" class="character_list-one js-li-character">
+      <img
+        class="char_img"
+        src=${newCharData.imageUrl}
+        alt="character"
+      />
+      <p class="char_name">${newCharData.name}</p>
+    </li>`;
+  return html;
+}
+
+function renderSearchList(filterList) {
+  charListElement.innerHTML = '';
+  for (const eachNewCharacter of filterList) {
+    charListElement.innerHTML += renderSearchCharacter(eachNewCharacter);
+  }
+  addEventCharacter();
+  //addDefaultImage();
+}
 
 searchBtn.addEventListener('click', handleSearch);
